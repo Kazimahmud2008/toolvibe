d4251ac1943c5b144be446bbbfdfd177fde78ae4
Kazimahmud2008 <kazimahmud835@gmail.com>
2025-07-29 22:59:13 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // ইম্পোর্ট যোগ করুন
-import App from './App';
-import QRCodePage from './pages/QRCode/QRCodePage'; // সঠিক পাথে ইম্পোর্ট
-import NotFound from './components/NotFound/NotFound'; // সঠিক পাথে ইম্পোর্ট
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/qr-code" element={<QRCodePage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+createRoot(document.getElementById("root")!).render(<App />);
